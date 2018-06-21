2acd82dbe1b64158b876e49f89351f8bba6e43e5
umxe <u_habiba@rocketmail.com>
2018-06-21 15:39:09 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,30 @@
 #write out your code here
 
+
 def least_coins(cents)
+  coins = ["quarters", "dimes", "nickels", "pennies"]
+  cents1 = []
+  quarter = cents/25; 
+  quarter_change = quarter*25; 
+  remChange = cents - quarter_change; 
+  dime = remChange/10; 
+  dime_change = dime*10; 
+  remChange1 = remChange - dime_change; 
+  nickel = remChange1/5; 
+  nickel_change = nickel*5; 
+  pennies = remChange1 - nickel_change; 
+  cents1.push(quarter)
+  cents1.push(dime)
+  cents1.push(nickel)
+  cents1.push(pennies)
+  change = {}
 
-#Code your answer here!
-
+  each_cent = 0
+  coins.each do |scent|
+    change[scent] = cents1[each_cent]
+    each_cent += 1
+  end
+  puts change
 end
+
+least_coins(94)
